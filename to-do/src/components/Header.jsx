4ea72d25792f6b0cac0d4b9c8a3989058e85fe1c
Akshay <mandaakshay20@gmail.com>
2024-12-15 16:56:42 +0530

    Made change in header.

--- to-do/src/components/Header.jsx
+++ to-do/src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = ({ toggleDarkMode, darkMode }) => {
   return (
     <header className="header">
-      <h1>Task Manager</h1>
+      <h1>To Do App</h1>
       <button onClick={toggleDarkMode}>
         {darkMode ? "Light Mode" : "Dark Mode"}
       </button>
